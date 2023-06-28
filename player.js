da0d8cbfcd2351f51e296a5cb4eabb150ff76fce
youssefsaleh61 <youssefsaleh61@yahoo.com>
2023-06-29 02:04:45 +0300

    Add collision checking

--- player.js
+++ player.js
@@ -1,9 +1,30 @@
 let player = document.getElementById("player");
+let solid = document.getElementById("solid");
 let keyState = {};
 
 let playerX = 0;
 let playerY = 0;
 let playerSpeed = 5;
+
+function checkCollision(obj1, obj2) {
+  // Get the bounding rectangles of the objects
+  let rect1 = obj1.getBoundingClientRect();
+  let rect2 = obj2.getBoundingClientRect();
+
+  // Check for collision
+  if (
+    rect1.left < rect2.right &&
+    rect1.right > rect2.left &&
+    rect1.top < rect2.bottom &&
+    rect1.bottom > rect2.top
+  ) {
+    // Collision detected
+    return true;
+  }
+
+  // No collision
+  return false;
+}
 
 function updatePlayerPosition() {
   if (keyState["KeyW"]) {
@@ -19,6 +40,11 @@ function updatePlayerPosition() {
     playerX += playerSpeed;
   }
   
+  if(checkCollision(player, solid))
+    console.log("Collision");
+  else
+    console.log("No Collision");
+
   player.style.transform = `translate(${playerX}px, ${playerY}px)`;
 
   requestAnimationFrame(updatePlayerPosition);
