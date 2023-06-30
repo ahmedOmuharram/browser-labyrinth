3ec6bed421e2645afe8e41d13b04d2335209b07b
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-06-30 19:46:53 +0300

    Restart Development

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("main");
+const ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.lineWidth = "40";
+ctx.strokeStyle = "#c3c3c3";
+ctx.rect(0, 0, 1280, 720);
+ctx.stroke();
+
