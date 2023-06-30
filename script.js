4cd333ccadd12d45f686aa3bc30c9ad608dfbbbf
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-06-30 20:08:31 +0300

    Install PixiJS

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const canvas = document.getElementById("main");
-const ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.lineWidth = "40";
-ctx.strokeStyle = "#c3c3c3";
-ctx.rect(0, 0, 1280, 720);
-ctx.stroke();
+let app = new PIXI.Application({ width: 1280, height: 720 });
+document.body.appendChild(app.view);
 
+// Create the sprite and add it to the stage
+let sprite = PIXI.Sprite.from('media/sprite.png');
+app.stage.addChild(sprite);
+
+// Add a ticker callback to move the sprite back and forth
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
