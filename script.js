cacc40aae8c488d71919f2f0d0ab0efeb436e047
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-07-01 22:05:38 +0300

    Add block.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-dragPoint = 0;
-
 const app = new PIXI.Application({ 
     antialias: true, 
     width: window.innerWidth, 
@@ -7,50 +5,6 @@ const app = new PIXI.Application({
     backgroundColor: "#008080",
     resolution: 1
 });
-
-class Block{
-  constructor(positionX, positionY, width, height, thickness, color){
-    this.positionX = positionX;
-    this.positionY = positionY;
-    this.width = width;
-    this.height = height;
-    this.thickness = thickness;
-    this.color = color;
-    this.graphic = new PIXI.Graphics();
-    this.graphic.interactive = true;
-    this.graphic.hitArea = new PIXI.Rectangle(positionX, positionY, width, height);
-    this.graphic.on("pointerdown", this.onDragStart);
-    this.graphic.on("pointerup", this.onDragEnd);
-    this.graphic.on("pointerupoutside", this.onDragEnd);
-    app.stage.addChild(this.graphic);
-  }
-  drawBlock(){
-    this.graphic.clear();
-    this.graphic.lineStyle(this.thickness, this.color).drawRect(this.positionX, this.positionY, this.width, this.height);
-    let bounds = this.graphic.getBounds()
-    this.graphic.hitArea = new PIXI.Rectangle(bounds.x, bounds.y, bounds.width, bounds.height);
-  }
-  onDragStart = (event) => {
-    event.stopPropagation();
-    dragPoint = event.data.getLocalPosition(this.graphic.parent);
-    this.graphic.parent.on("pointermove", this.onDragMove);
-  };
-  
-  onDragMove = (event) => {
-      const newPoint = event.data.getLocalPosition(this.graphic.parent);
-      const offsetX = newPoint.x - dragPoint.x;
-      const offsetY = newPoint.y - dragPoint.y;
-      this.positionX += offsetX;
-      this.positionY += offsetY;
-      dragPoint.x = newPoint.x;
-      dragPoint.y = newPoint.y;
-  };
-  
-  onDragEnd = (event) => {
-    event.stopPropagation();
-    this.graphic.parent.off("pointermove", this.onDragMove);
-  };
-}
 
 document.body.appendChild(app.view);
 
