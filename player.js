8548b104620f8611ea9ba351840773f17e5bc656
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-07-01 21:55:53 +0300

    Refactor code

--- player.js
+++ player.js
@@ -1,273 +1,97 @@
-let keyState = {};
+const gravity = 0.1;
+const terminalVelocity = 1000000;
+let isOnGround = true;
+const playerJumpForce = 5;
 
-let playerX = 0;
-let playerY = 0;
-let playerSpeed = 300;
+const playerSprite = PIXI.Sprite.from('media/sprite.png');
+playerSprite.anchor.set(0.5);
+playerSprite.x = app.screen.width / 2;
+playerSprite.y = 500;
+playerSprite.xSpeed = 0;
+playerSprite.ySpeed = 0;
+playerSprite.width = 36;
+playerSprite.height = 40;
 
-let gravity = 1000;
-let verticalVelocity = 0;
-let jumpForce = 500;
-let isOnGround = false;
+app.stage.addChild(playerSprite);
 
-let currentLevel = 0;
+const keys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false
+};
 
-const targetFPS = 1001;
-const targetInterval = 1000 / targetFPS;
+const speed = 5;
 
-let previousTimestamp = 0;
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
 
-
-function createCustomBody(bottomValue, rightValue, widthValue, heightValue) {
-  document.body.innerHTML += `<div class="object solid" style="position:absolute; box-shadow: 2px 3px; border: 1px solid white; background-color: #c3c3c3; ${bottomValue}px; ${rightValue}px; width: ${widthValue}; height: ${heightValue}"></div>`;
-}
-
-//Main Menu
-function createMainMenu(){
-  createCustomBody('bottom: 200', 'right: 50', '20px', '500px');
-  createCustomBody('bottom: 100', 'right: 500', '20px', '500px');
-  createCustomBody('bottom: 400', 'right: 800', '500px', '20px');
-  createCustomBody('bottom: 600', 'right: 300', '400px', '20px');
-  playerX = 0.5 * window.innerWidth - 610;
-  playerY = 0.5 * window.innerHeight + 330;
-}
-
-//Level 1
- function createLevel1(){
-   createCustomBody('top: 100', 'left: 600', '300px', '100px');
-   createCustomBody('bottom : 1', 'left: 730', '30px', '90%');
-   playerX = 0.5 * window.innerWidth - 610;
-   playerY = 0.5 * window.innerHeight + 330;
- }
-
-//Level 2
- function createLevel2(){
-   for (var i = 1; i <= 14; i++) {
-     if (i % 2 == 0) {
-       console.log()
-       createCustomBody('top: -1', `left: ${i}00`, '2vw', 'calc(200vh - 40vw)');
-     } else {
-       createCustomBody('bottom: -1', `left: ${i}00`, '2vw', 'calc(200vh - 40vw)');
-     }
-   }
-   playerX = 0.5 * window.innerWidth - 610;
-   playerY = 0.5 * window.innerHeight + 330;
-}
-
-//Level 3
-function createLevel3(){
-  for (var i = 2; i <= 7; i++) {
-    if (i % 2 == 0) {
-      createCustomBody(`bottom: ${i}00`, 'left: 800', '1500px', '130px');
-    } else {
-      createCustomBody(`bottom: ${i}00`, 'right: 800', '1500px', '130px');
+function onKeyDown(event) {
+    const key = event.key.toLowerCase();
+    if (keys.hasOwnProperty(key)) {
+        keys[key] = true;
     }
-  }
-  createCustomBody('top: 100', 'right: 700', '300px', '100px');
 }
 
-//Level 4
-function createLevel4(){
-  for (var i = 1; i <= 6; i++) {
-    createCustomBody(`bottom: ${i}00`, `left: ${-i}00`, `${2*i+4}00px`, '30px');
-    createCustomBody(`bottom: ${i}00`, `left: ${i+4}70`, '1500px', '30px');
-    createCustomBody(`top: ${-i}00`, `right: ${i+3}00`, '30px', `${2*i}00px`);
-    createCustomBody(`top: ${i}70`, `right: ${i+3}00`, '30px', '700px');  
-  }
-}
-
-function createLevel5(){
-  createCustomBody('top: 100', 'left: 700', '300px', '100px');
-}
-
-function removeLevel(){
-  for(var i = 4; i < solids.length; i++){
-    solids[i].remove();
-  }
-}
-
-function nextLevel(){
-  removeLevel();
-  switch (currentLevel){
-    case 0:
-      createLevel1();
-      break;
-    case 1:
-      createLevel2();
-      break;
-    case 2:
-      createLevel3();
-      break;
-    case 3:
-      createLevel4();
-      break;
-    case 4:
-      createLevel5();
-      break;
-  }
-  currentLevel++;
-  player = document.getElementById("player");
-  solids = document.querySelectorAll('.solid');
-}
-
-function setLevel(inputLevel){
-  removeLevel();
-  switch (inputLevel){
-    case 0:
-      createMainMenu();
-      break;
-    case 1:
-      createLevel1();
-      break;
-    case 2:
-      createLevel2();
-      break;
-    case 3:
-      createLevel3();
-      break;
-    case 4:
-      createLevel4();
-      break;
-  }
-  player = document.getElementById("player");
-  solids = document.querySelectorAll('.solid');
-}
-
-function previousLevel(){
-  removeLevel();
-  switch (currentLevel){
-    case 1:
-      createMainMenu();
-      break;
-    case 2:
-      createLevel1();
-      break;
-    case 3:
-      createLevel2();
-      break;
-    case 4:
-      createLevel3();
-      break;
-    case 5:
-      createLevel4();
-      break;
-  }
-  currentLevel--;
-  player = document.getElementById("player");
-  solids = document.querySelectorAll('.solid');
-}
-
-
-function createRamp(bottomValue, rightValue, widthValue, heightValue) {
-  document.body.innerHTML += `<div class="object solid" style="border-radius: 0% 100% 0% 0%; box-shadow: 2px 3px; position:absolute; border: 1px solid white; background-color: #c3c3c3; bottom: ${bottomValue}px; right: ${rightValue}px; width: ${widthValue}px; height: ${heightValue}px"></div>`;
-}
-
-createMainMenu();
-
-let startButton = document.getElementById("start")
-
-let player = document.getElementById("player");
-let solids = document.querySelectorAll('.solid');
-
-function checkCollision(obj1, obj2) {
-  let rect1 = obj1.getBoundingClientRect();
-  let rect2 = obj2.getBoundingClientRect();
-
-  if (
-    rect1.left < rect2.right &&
-    rect1.right > rect2.left &&
-    rect1.top < rect2.bottom &&
-    rect1.bottom > rect2.top
-  ) { return true; }
-
-  return false;
-}
-
-function gameLoop(timestamp) {
-    const elapsed = timestamp - previousTimestamp;
-    const deltaTime = elapsed / 1000;
-
-    verticalVelocity += gravity * deltaTime;
-    playerY += verticalVelocity * deltaTime;
-    
-    // if (keyState["KeyW"]) {
-    //     playerY -= playerSpeed * deltaTime;
-    // }
-    if (keyState["KeyA"]) {
-        playerX -= playerSpeed * deltaTime;
+function onKeyUp(event) {
+    const key = event.key.toLowerCase();
+    if (keys.hasOwnProperty(key)) {
+        keys[key] = false;
     }
-    // if (keyState["KeyS"]) {
-    //     playerY += playerSpeed * deltaTime;
-    // }
-    if (keyState["KeyD"]) {
-        playerX += playerSpeed * deltaTime;
+}
+
+app.ticker.add(gameLoop);
+
+function gameLoop(delta) {
+    playerSprite.ySpeed += gravity;
+    playerSprite.xSpeed = 0;
+    if (playerSprite.ySpeed > terminalVelocity) {
+        playerSprite.ySpeed = terminalVelocity;
     }
 
-    if(keyState["Enter"])
-    if(checkCollision(player, startButton)){
-      nextLevel();
+    if (keys.w && isOnGround) {
+        playerSprite.ySpeed = -playerJumpForce;
+        isOnGround = false;
     }
-    let colliding = false;
-    for( var i = 0; i < solids.length; i++) {
-      if (checkCollision(player, solids[i])) {
-        colliding = true;
-        resolveCollision(player.getBoundingClientRect(), solids[i].getBoundingClientRect());
-        solids[i].style.backgroundColor = "#ff0081"
-      } else {
-        if (solids[i].id == "start") {
-          solids[i].style.backgroundColor = "#010081"
-        } else {
-          solids[i].style.backgroundColor = "#c3c3c3"
+    playerSprite.xSpeed -= keys.a * speed;
+    playerSprite.xSpeed += keys.d * speed;
+
+    playerSprite.x += playerSprite.xSpeed;
+    playerSprite.y += playerSprite.ySpeed;
+    colliding = false;
+    for (var i = 0; i < blocks.length; i++) {
+        if (isColliding(playerSprite, blocks[i].graphic)) {
+            resolveCollision(playerSprite, blocks[i].graphic);
+            colliding = true;
         }
-      }
     }
 
     if (!colliding) {
-      gravity = 1000;
+        isOnGround = false;
     }
-
-    if (playerY > 0.5 * window.innerHeight + 340 || playerY < 0.5 * window.innerHeight - 380
-      || playerX > 0.5 * window.innerWidth + 660 || playerX < 0.5 * window.innerWidth - 660) {
-        kill()
-    }
-
-    if (playerX < 0) {
-      playerX = 0;
-    }
-
-    if (playerY < 0) {
-        playerY = 0;
-    }
-    
-    if (playerX + player.offsetWidth > window.innerWidth) {
-        playerX = window.innerWidth - player.offsetWidth;
-        isOnGround = true;
-    }
-    
-    if (playerY + player.offsetHeight > window.innerHeight) {
-        playerY = window.innerHeight - player.offsetHeight;
-        verticalVelocity = 0;
-        isOnGround = true;
-    }
-    if(window.innerHeight < 70){
-      player.remove();
-    }
-
-    player.style.transform = `translate(${playerX}px, ${playerY}px)`;
-
-
-    previousTimestamp = timestamp;
-    setTimeout(() => {
-        requestAnimationFrame(gameLoop);
-    }, targetInterval);
 }
 
-function resolveCollision(playerRect, solidRect) {
+function isColliding(sprite, rect) {
+    const spriteBounds = sprite.getBounds();
+    const rectBounds = rect.getLocalBounds();
 
-  const dx = (playerRect.left + playerRect.right) / 2 - (solidRect.left + solidRect.right) / 2;
-  const dy = (playerRect.top + playerRect.bottom) / 2 - (solidRect.top + solidRect.bottom) / 2;
+    return (
+        spriteBounds.x + spriteBounds.width >= rectBounds.x &&
+        spriteBounds.x <= rectBounds.x + rectBounds.width &&
+        spriteBounds.y + spriteBounds.height >= rectBounds.y &&
+        spriteBounds.y <= rectBounds.y + rectBounds.height
+    );
+}
 
-  const combinedHalfWidths = (playerRect.width + solidRect.width) / 2;
-  const combinedHalfHeights = (playerRect.height + solidRect.height) / 2;
+function resolveCollision(sprite, rect) {
+  const spriteBounds = sprite.getBounds();
+  const rectBounds = rect.getLocalBounds();
+
+  const dx = (spriteBounds.x + spriteBounds.x + spriteBounds.width) / 2 - (rectBounds.x + rectBounds.x + rectBounds.width) / 2;
+  const dy = (spriteBounds.y + spriteBounds.y + spriteBounds.height) / 2 - (rectBounds.y + rectBounds.y + rectBounds.height) / 2;
+
+  const combinedHalfWidths = (spriteBounds.width + rectBounds.width) / 2;
+  const combinedHalfHeights = (spriteBounds.height + rectBounds.height) / 2;
 
   if (Math.abs(dx) < combinedHalfWidths && Math.abs(dy) < combinedHalfHeights) {
     const overlapX = combinedHalfWidths - Math.abs(dx);
@@ -276,63 +100,28 @@ function resolveCollision(playerRect, solidRect) {
     if (overlapX >= overlapY) {
       if (dy > 0) {
         //Player top side collision
-        playerY += overlapY;
-        verticalVelocity = 0;
-      } else {
+        playerSprite.y += overlapY;
+        playerSprite.ySpeed += gravity;
+    } else {
         //Player bottom side collision
-        if (verticalVelocity > 0) {
-          verticalVelocity = 0;
+        if (playerSprite.ySpeed > 0) {
+          playerSprite.ySpeed = 0;
         }
         isOnGround = true;
-        playerY -= overlapY;
+        playerSprite.y -= overlapY;
       }
       return true;
     } else {
       if (dx > 0) {
         //Player left side collision
-        playerX += overlapX;
+        playerSprite.x += overlapX;
         isOnGround = true;
       } else {
         //Player right side collision
-        playerX -= overlapX;
+        playerSprite.x -= overlapX;
         isOnGround = true;
       }
       return true;
     }
   }
 }
-
-function jump() {
-    if (isOnGround) { 
-      verticalVelocity = -jumpForce;
-      isOnGround = false;
-    }
-}
-
-function kill() {
-  setLevel(currentLevel);
-  playerX = 0.5 * window.innerWidth - 610;
-  playerY = 0.5 * window.innerHeight + 330;
-  console.log(currentLevel)
-  player = document.getElementById("player");
-  solids = document.querySelectorAll('.solid');
-}
-
-document.addEventListener("keydown", (event) => {
-  keyState[event.code] = true;
-  if (event.code === "Space") {
-    jump();
-  }
-  if (event.code === "KeyN") {
-    nextLevel();
-  }
-  if (event.code === "KeyP") {
-    previousLevel();
-  }
-});
-
-document.addEventListener("keyup", (event) => {
-  keyState[event.code] = false;
-});
-
-requestAnimationFrame(gameLoop);
--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-const gravity = 0.1;
-const terminalVelocity = 1000000;
-var colliding = false;
-
-topBorderY = 0
-bottomBorderY = 0
-leftBorderX = 0
-rightBorderX = 0
-
+dragPoint = 0;
 
 const app = new PIXI.Application({ 
     antialias: true, 
@@ -15,328 +7,71 @@ const app = new PIXI.Application({
     backgroundColor: "#008080",
     resolution: 1
 });
-    
+
+class Block{
+  constructor(positionX, positionY, width, height, thickness, color){
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.width = width;
+    this.height = height;
+    this.thickness = thickness;
+    this.color = color;
+    this.graphic = new PIXI.Graphics();
+    this.graphic.interactive = true;
+    this.graphic.hitArea = new PIXI.Rectangle(positionX, positionY, width, height);
+    this.graphic.on("pointerdown", this.onDragStart);
+    this.graphic.on("pointerup", this.onDragEnd);
+    this.graphic.on("pointerupoutside", this.onDragEnd);
+    app.stage.addChild(this.graphic);
+  }
+  drawBlock(){
+    this.graphic.clear();
+    this.graphic.lineStyle(this.thickness, this.color).drawRect(this.positionX, this.positionY, this.width, this.height);
+    let bounds = this.graphic.getBounds()
+    this.graphic.hitArea = new PIXI.Rectangle(bounds.x, bounds.y, bounds.width, bounds.height);
+  }
+  onDragStart = (event) => {
+    event.stopPropagation();
+    dragPoint = event.data.getLocalPosition(this.graphic.parent);
+    this.graphic.parent.on("pointermove", this.onDragMove);
+  };
+  
+  onDragMove = (event) => {
+      const newPoint = event.data.getLocalPosition(this.graphic.parent);
+      const offsetX = newPoint.x - dragPoint.x;
+      const offsetY = newPoint.y - dragPoint.y;
+      this.positionX += offsetX;
+      this.positionY += offsetY;
+      dragPoint.x = newPoint.x;
+      dragPoint.y = newPoint.y;
+  };
+  
+  onDragEnd = (event) => {
+    event.stopPropagation();
+    this.graphic.parent.off("pointermove", this.onDragMove);
+  };
+}
+
 document.body.appendChild(app.view);
 
 app.stage.interactive = true;
 app.stage.hitArea = app.screen;
 
-let topBorder = new PIXI.Graphics();
-let bottomBorder = new PIXI.Graphics();
-let leftBorder = new PIXI.Graphics();
-let rightBorder = new PIXI.Graphics();
+let topBorder = new Block(window.innerWidth/2 - 640, window.innerHeight/2 - 360, 1300, 20, 10, "#FF0000");
+let bottomBorder = new Block(window.innerWidth/2 - 640, window.innerHeight/2 + 360, 1300, 20, 10, "#00FF00");
+let leftBorder = new Block(window.innerWidth/2 - 640, window.innerHeight/2 - 360, 20, 740, 10, "#0000FF");
+let rightBorder = new Block(window.innerWidth/2 + 640, window.innerHeight/2 - 360, 20, 740, 10, "#FF00FF");
 
-topBorder.interactive = true;
-bottomBorder.interactive = true;
-leftBorder.interactive = true;
-rightBorder.interactive = true;
-
-topBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 - 640, window.innerHeight/2 - 360 + topBorderY, 1300, 20);
-bottomBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 - 640, window.innerHeight/2 + 360 + bottomBorderY, 1300, 20);
-leftBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 - 640 + leftBorderX, window.innerHeight/2 - 360, 20, 740);
-rightBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 + 640 + rightBorderX, window.innerHeight/2 - 360, 20, 740);
-
-const tonDragStart = (event) => {
-  event.stopPropagation();
-  dragPoint = event.data.getLocalPosition(topBorder.parent);
-  dragPoint.y -= topBorder.y;
-  topBorder.parent.on("pointermove", tonDragMove);
-};
-
-const tonDragMove = (event) => {
-    const newPoint = event.data.getLocalPosition(topBorder.parent);
-    const offsetY = newPoint.y - dragPoint.y;
-    topBorderY += offsetY;
-    dragPoint.y = newPoint.y;
-    topBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 - 640, window.innerHeight/2 - 360 + topBorderY, 1300, 20);
-};
-
-const tonDragEnd = (event) => {
-  event.stopPropagation();
-  topBorder.parent.off("pointermove", tonDragMove);
-};
-
-const bonDragStart = (event) => {
-  event.stopPropagation();
-  dragPoint = event.data.getLocalPosition(bottomBorder.parent);
-  dragPoint.y -= bottomBorder.y;
-  bottomBorder.parent.on("pointermove", bonDragMove);
-};
-
-const bonDragMove = (event) => {
-    const newPoint = event.data.getLocalPosition(bottomBorder.parent);
-    const offsetY = newPoint.y - dragPoint.y;
-    bottomBorderY += offsetY;
-    dragPoint.y = newPoint.y;
-    bottomBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 - 640, window.innerHeight/2 + 360 + bottomBorderY, 1300, 20);
-};
-
-const bonDragEnd = (event) => {
-  event.stopPropagation();
-  topBorder.parent.off("pointermove", bonDragMove);
-};
-
-const lonDragStart = (event) => {
-  event.stopPropagation();
-  dragPoint = event.data.getLocalPosition(leftBorder.parent);
-  dragPoint.x -= leftBorder.x;
-  leftBorder.parent.on("pointermove", lonDragMove);
-};
-
-const lonDragMove = (event) => {
-    const newPoint = event.data.getLocalPosition(leftBorder.parent);
-    const offsetX = newPoint.x - dragPoint.x;
-    leftBorderX += offsetX;
-    dragPoint.x = newPoint.x;
-    leftBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 - 640 + leftBorderX, window.innerHeight/2 - 360, 20, 740);
-};
-
-const lonDragEnd = (event) => {
-  event.stopPropagation();
-  topBorder.parent.off("pointermove", lonDragMove);
-};
-
-
-const ronDragStart = (event) => {
-  event.stopPropagation();
-  dragPoint = event.data.getLocalPosition(rightBorder.parent);
-  dragPoint.x -= rightBorder.x;
-  dragPoint.y -= rightBorder.y;
-  rightBorder.parent.on("pointermove", ronDragMove);
-};
-
-const ronDragMove = (event) => {
-    const newPoint = event.data.getLocalPosition(rightBorder.parent);
-    const offsetX = newPoint.x - dragPoint.x;
-    rightBorderX += offsetX;
-    dragPoint.x = newPoint.x;
-    rightBorder.hitArea = new PIXI.Rectangle(window.innerWidth/2 + 640 + rightBorderX, window.innerHeight/2 - 360, 20, 740);
-};
-
-const ronDragEnd = (event) => {
-  event.stopPropagation();
-  topBorder.parent.off("pointermove", ronDragMove);
-};
-
-topBorder.on("pointerdown", tonDragStart);
-topBorder.on("pointerup", tonDragEnd);
-topBorder.on("pointerupoutside", tonDragEnd);
-
-bottomBorder.on("pointerdown", bonDragStart);
-bottomBorder.on("pointerup", bonDragEnd);
-bottomBorder.on("pointerupoutside", bonDragEnd);
-
-leftBorder.on("pointerdown", lonDragStart);
-leftBorder.on("pointerup", lonDragEnd);
-leftBorder.on("pointerupoutside", lonDragEnd);
-
-rightBorder.on("pointerdown", ronDragStart);
-rightBorder.on("pointerup", ronDragEnd);
-rightBorder.on("pointerupoutside", ronDragEnd);
-
-/*let box = new PIXI.Graphics();
-box.lineStyle(20, "#ff0000").drawRect(window.innerWidth/2 - 640, window.innerHeight/2 - 360, 1280, 720);
-
-
-blocks.push(box)
-app.stage.addChild(box);*/
 blocks = []
-
 blocks.push(topBorder);
 blocks.push(bottomBorder);
 blocks.push(leftBorder);
 blocks.push(rightBorder);
-
-app.stage.addChild(topBorder);
-app.stage.addChild(bottomBorder);
-app.stage.addChild(leftBorder);
-app.stage.addChild(rightBorder);
-
-const playerSprite = PIXI.Sprite.from('media/sprite.png');
-
-// center the sprite's anchor point
-playerSprite.anchor.set(0.5);
-
-// move the sprite to the center of the screen
-playerSprite.x = app.screen.width / 2;
-playerSprite.y = 500;
-playerSprite.xSpeed = 0;
-playerSprite.ySpeed = 0;
-playerSprite.width = 36;
-playerSprite.height = 40;
-let isOnGround = true;
-const playerJumpForce = 5;
-
-app.stage.addChild(playerSprite);
-
-const keys = {
-    w: false,
-    a: false,
-    s: false,
-    d: false
-  };
   
-  const speed = 5;
-  
-  document.addEventListener('keydown', onKeyDown);
-  document.addEventListener('keyup', onKeyUp);
-  
-  function onKeyDown(event) {
-    const key = event.key.toLowerCase();
-    if (keys.hasOwnProperty(key)) {
-      keys[key] = true;
-    }
-  }
-  
-  function onKeyUp(event) {
-    const key = event.key.toLowerCase();
-    if (keys.hasOwnProperty(key)) {
-      keys[key] = false;
-    }
-  }
-  
-  app.ticker.add(gameLoop);
+app.ticker.add(gameLoop);
   
 function gameLoop(delta) {
-    topBorder.updateTransform();
-    drawObjects();
-    playerSprite.ySpeed += gravity;
-    playerSprite.xSpeed = 0;
-    if (playerSprite.ySpeed > terminalVelocity) {
-        playerSprite.ySpeed = terminalVelocity;
-    }
-
-    if(keys.w && isOnGround){
-      playerSprite.ySpeed = -playerJumpForce;
-      isOnGround = false;
-    }
-    playerSprite.xSpeed -= keys.a*speed;
-    playerSprite.xSpeed += keys.d*speed;
-
-    playerSprite.x += playerSprite.xSpeed;
-    playerSprite.y += playerSprite.ySpeed;
-    colliding = false;
-    for (var i = 0; i < blocks.length; i++) {
-        if (isColliding(playerSprite, blocks[i])) {
-            console.log("test")
-           resolveCollision(playerSprite, blocks[i]);
-           colliding = true;
-        }
-    }
-
-    if (!colliding) {
-        isOnGround = false;
-    }
-}
-/*
-function isCollidingBox(sprite, rect) {
-    const spriteBounds = sprite.getBounds();
-    const rectBounds = rect.getBounds();
-  
-    const borderThickness = 20;
-  
-    const spriteRight = spriteBounds.x + spriteBounds.width;
-    const spriteBottom = spriteBounds.y + spriteBounds.height;
-    const rectRight = rectBounds.x + rectBounds.width;
-    const rectBottom = rectBounds.y + rectBounds.height;
-  
-    // Check if any of the sprite's borders overlap with the rectangle's borders
-    const collidingLeft = spriteRight > rectBounds.x && spriteBounds.x < rectBounds.x + borderThickness;
-    const collidingRight = spriteBounds.x < rectRight && spriteRight > rectRight - borderThickness;
-    const collidingTop = spriteBottom > rectBounds.y && spriteBounds.y < rectBounds.y + borderThickness;
-    const collidingBottom = spriteBounds.y < rectBottom && spriteBottom > rectBottom - borderThickness;
-  
-    return collidingLeft || collidingRight || collidingTop || collidingBottom;
-}
-
-function resolveBoxCollision(sprite, rect) {
-    const spriteBounds = sprite.getBounds();
-    const rectBounds = rect.getLocalBounds();
-    const rectThickness = 20
-    
-    const left = rectBounds.x;
-    const right = rectBounds.x + rectBounds.width;
-    const top = rectBounds.y;
-    const bottom = rectBounds.y + rectBounds.height;
-
-    if (spriteBounds.x <= left+rectThickness) {
-        playerSprite.x = left+spriteBounds.width/2+rectThickness;
-    }
-    if (spriteBounds.x+spriteBounds.width >= right-rectThickness) {
-        playerSprite.x = right-spriteBounds.width/2-rectThickness;
-    }
-
-    if (spriteBounds.y+spriteBounds.height >= bottom-rectThickness) {
-        isOnGround = true;
-        playerSprite.y = bottom-spriteBounds.height/2-rectThickness;
-    }
-    
-    if (spriteBounds.y <= top+rectThickness) {
-        playerSprite.y = top+spriteBounds.height/2+rectThickness;
-    }
-}
-*/
-
-function isColliding(sprite, rect) {
-  const spriteBounds = sprite.getBounds();
-  const rectBounds = rect.getLocalBounds();
-
-  return spriteBounds.x + spriteBounds.width >= rectBounds.x &&
-         spriteBounds.x <= rectBounds.x + rectBounds.width &&
-         spriteBounds.y + spriteBounds.height >= rectBounds.y &&
-         spriteBounds.y <= rectBounds.y + rectBounds.height;
-}
-
-function resolveCollision(sprite, rect) {
-  const spriteBounds = sprite.getBounds();
-  const rectBounds = rect.getLocalBounds();
-
-  const dx = (spriteBounds.x + spriteBounds.x + spriteBounds.width) / 2 - (rectBounds.x + rectBounds.x + rectBounds.width) / 2;
-  const dy = (spriteBounds.y + spriteBounds.y + spriteBounds.height) / 2 - (rectBounds.y + rectBounds.y + rectBounds.height) / 2;
-
-  const combinedHalfWidths = (spriteBounds.width + rectBounds.width) / 2;
-  const combinedHalfHeights = (spriteBounds.height + rectBounds.height) / 2;
-
-  if (Math.abs(dx) < combinedHalfWidths && Math.abs(dy) < combinedHalfHeights) {
-    const overlapX = combinedHalfWidths - Math.abs(dx);
-    const overlapY = combinedHalfHeights - Math.abs(dy);
-
-    if (overlapX >= overlapY) {
-      if (dy > 0) {
-        //Player top side collision
-        playerSprite.y += overlapY;
-        playerSprite.ySpeed += gravity;
-    } else {
-        //Player bottom side collision
-        if (playerSprite.ySpeed > 0) {
-          playerSprite.ySpeed = 0;
-        }
-        isOnGround = true;
-        playerSprite.y -= overlapY;
-      }
-      return true;
-    } else {
-      if (dx > 0) {
-        //Player left side collision
-        playerSprite.x += overlapX;
-        isOnGround = true;
-      } else {
-        //Player right side collision
-        playerSprite.x -= overlapX;
-        isOnGround = true;
-      }
-      return true;
-    }
-  }
-}
-
-function drawObjects(){
-  topBorder.clear();
-  bottomBorder.clear();
-  leftBorder.clear();
-  rightBorder.clear();
-  topBorder.lineStyle(2, "#ff0000").drawRect(window.innerWidth/2 - 640, window.innerHeight/2 - 360 + topBorderY, 1300, 20);
-  bottomBorder.lineStyle(2, "#00ff00").drawRect(window.innerWidth/2 - 640, window.innerHeight/2 + 360 + bottomBorderY, 1300, 20);
-  leftBorder.lineStyle(2, "#0000ff").drawRect(window.innerWidth/2 - 640 + leftBorderX, window.innerHeight/2 - 360, 20, 740);
-  rightBorder.lineStyle(2, "#ff00ff").drawRect(window.innerWidth/2 + 640 + rightBorderX, window.innerHeight/2 - 360, 20, 740);
+    blocks.forEach(block => {
+      block.drawBlock();
+    });
 }
