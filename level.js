b5fbdb660f82bce7d03bc4b128fe062d55b58fd4
ahmedOmuharram <ahmed.o.muharram@gmail.com>
2023-07-02 20:27:20 +0300

    Add levelData.json; Add level generate function

--- level.js
+++ level.js
@@ -3,6 +3,7 @@ class Level{
     this.name = name;
     this.index = index;
     this.blocks = [];
+    this.characteristics = [];
   }
 
   addBlock(x, y, width, height, thickness, color, fillColor, borders) {
@@ -28,6 +29,44 @@ class Level{
                 break;     
         }
     }
+    return block
+  }
+
+  refresh() {
+    topBorder.positionX = window.innerWidth/2 - 640;
+    topBorder.positionY = window.innerHeight/2 - 360;
+
+    bottomBorder.positionX = window.innerWidth/2 - 640;
+    bottomBorder.positionY = window.innerHeight/2 + 360;
+
+    leftBorder.positionX = window.innerWidth/2 - 640;
+    leftBorder.positionY = window.innerHeight/2 - 360;
+
+    rightBorder.positionX = window.innerWidth/2 + 640;
+    rightBorder.positionY = window.innerHeight/2 - 360;
+    
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+  }
+
+  generate() {
+    fetch('levelData.json')
+    .then(response => response.json())
+    .then(data => {
+        var levelName = this.name;
+        var jsonData = Array.isArray(data) ? data : [data];
+        jsonData.forEach(obj => {
+            obj[levelName].forEach(block => {
+                var x = eval(block.x);
+                var y = eval(block.y);
+                var width = eval(block.width);
+                var height = eval(block.height);
+                block = this.addBlock(x, y, width, height, block.thickness, block.color, block.fillColor, block.borders);
+                levelBlocks.push(block)        
+            });
+        });
+    }).catch(error => {
+        console.error('Error:', error);
+    });
   }
 }
 
--- script.js
+++ script.js
@@ -11,53 +11,25 @@ document.body.appendChild(app.view);
 app.stage.interactive = true;
 app.stage.hitArea = app.screen;
 
-let topBorder = new Border(window.innerWidth/2 - 640, window.innerHeight/2 - 360, 1300, 20, 2, "#FF0000", 'v', "#FF0000");
-let bottomBorder = new Border(window.innerWidth/2 - 640, window.innerHeight/2 + 360, 1300, 20, 2, "#00FF00", 'v', "#00FF00");
-let leftBorder = new Border(window.innerWidth/2 - 640, window.innerHeight/2 - 360, 20, 740, 2, "#0000FF", 'h', "#0000FF");
-let rightBorder = new Border(window.innerWidth/2 + 640, window.innerHeight/2 - 360, 20, 740, 2, "#FF00FF", 'h', "#FF00FF");
+let bottomBorder = new Border(window.innerWidth/2 - 640, window.innerHeight/2 + 360, 1300, 20, 2, "#c8c8c8", 'v', "#c8c8c8");
+let leftBorder = new Border(window.innerWidth/2 - 640, window.innerHeight/2 - 360, 10, 740, 2, "#c8c8c8", 'h', "#c8c8c8");
+let rightBorder = new Border(window.innerWidth/2 + 640, window.innerHeight/2 - 360, 10, 740, 2, "#c8c8c8", 'h', "#c8c8c8");
+let topBorder = new Border(window.innerWidth/2 - 640, window.innerHeight/2 - 360, 1300, 20, 2, "#c8c8c8", 'v', "#010081");
 
-function refresh(){
-    topBorder.positionX = window.innerWidth/2 - 640;
-    topBorder.positionY = window.innerHeight/2 - 360;
+blocks = [topBorder, bottomBorder, leftBorder, rightBorder]
 
-    bottomBorder.positionX = window.innerWidth/2 - 640;
-    bottomBorder.positionY = window.innerHeight/2 + 360;
-
-    leftBorder.positionX = window.innerWidth/2 - 640;
-    leftBorder.positionY = window.innerHeight/2 - 360;
-
-    rightBorder.positionX = window.innerWidth/2 + 640;
-    rightBorder.positionY = window.innerHeight/2 - 360;
-}
-
-blocks = []
-
-
-let test = new Level("menu", 0);
-test.addBlock(leftBorder.positionX + 150, leftBorder.positionY, leftBorder.width, 3 * bottomBorder.width/4, 2, "#FFF", "#FFF", "tl");
-test.addBlock(window.innerWidth/2 - 50, window.innerHeight/2 + 50, leftBorder.positionX/2, topBorder.positionY/2, 2, "#FFF", "#FFF", "tr")
-test.addBlock(window.innerWidth/2 - 50, window.innerHeight/2 + 50, leftBorder.positionX/2, topBorder.positionY/2, 2, "#FFF", "#FFF", "bl")
-test.addBlock(window.innerWidth/2 - 50, window.innerHeight/2 + 50, leftBorder.positionX/2, topBorder.positionY/2, 2, "#FFF", "#FFF", "br")
-
-blocks.push(topBorder);
-blocks.push(bottomBorder);
-blocks.push(leftBorder);
-blocks.push(rightBorder);
-
+let test = new Level("test", 0);
+levelBlocks = []
+test.generate()
 
 app.ticker.add(gameLoop);
 
 window.onresize = function()
 {
-    refresh()
-    app.renderer.resize(window.innerWidth, window.innerHeight);
+    test.refresh();
 }
 
 function gameLoop(delta) {
-    blocks.forEach(block => {
-      block.drawBlock();
-    });
-
     if (topBorder.rechanging) {
         topBorder.positionX = leftBorder.positionX;
         topBorder.width = rightBorder.positionX - leftBorder.positionX + rightBorder.width/2 + leftBorder.width/2;
@@ -74,5 +46,10 @@ function gameLoop(delta) {
         rightBorder.positionY = topBorder.positionY;
         rightBorder.height = bottomBorder.positionY - topBorder.positionY + bottomBorder.height/2 + topBorder.height/2;
     }
-    test.blocks[0].height = 3 * bottomBorder.width/4;
+    if (levelBlocks[0]) {
+        levelBlocks[0].height = Math.min(leftBorder.height, 3 * bottomBorder.width/4);
+    }
+    blocks.forEach(block => {
+        block.drawBlock();
+    });
 }
