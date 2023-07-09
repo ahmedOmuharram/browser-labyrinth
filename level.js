2c103c6bcb2ad1cd2b25e69cd4b8961b2f101d37
youssefsaleh61 <youssefsaleh61@yahoo.com>
2023-07-09 22:17:11 +0300

    Change level order

--- level.js
+++ level.js
@@ -59,25 +59,61 @@ class Level{
         console.error('Error:', error);
     });
   }
-  Level0(delta){
+Level0(delta){
     if (animationStarted)
-    elapsed += delta;
+        elapsed += delta;
     if (elapsed > 50)
         elapsed = 50;
-    //topBorder.positionY = 340 - Math.sin(elapsed/(200/Math.PI)) * 340;
     bottomBorder.positionY = 20 + Math.sin(elapsed/(100/Math.PI)) * 690;
     rightBorder.positionX = 10 + Math.sin(elapsed/(100/Math.PI)) * 1260;
-    //leftBorder.positionX = 630 - Math.sin(elapsed/(200/Math.PI)) * 630;
     if (elapsed >= 50)
         setLevel(1);
-  }
-  Level1(delta){
+}
+Level1(delta){
 
-   }
-  Level2(delta){
+}
+Level2(delta){
     
-  }
-  Level3(delta){
+}
+Level3(delta){
+    for (let i = 0; i < 6; i++) {
+        if (levelBlocks[i]) {
+            levelBlocks[i].positionX += 5 * delta;
+        }
+    }
+    for (let i = 6; i < 12; i++) {
+        if (levelBlocks[i]) {
+            levelBlocks[i].positionX -= 5 * delta;
+        }
+    }
+}
+
+Level4(delta){
+    elapsed += delta;
+    for (let i = 0; i < 52; i++) {
+        if (levelBlocks[i]) {
+            if (i < 16) {
+                levelBlocks[i].positionX = i * 20.0 * Math.cos((elapsed * i/100.0))
+                levelBlocks[i].positionY = i * 20.0 * Math.sin((elapsed * i/100.0))
+            } else if (i < 32) {
+                levelBlocks[i].positionX = screenWidth + (i - 16) * 20.0 * Math.cos((elapsed * (i - 16)/100.0))
+                levelBlocks[i].positionY = screenHeight + (i - 16) * 20.0 * Math.sin((elapsed * (i - 16)/100.0))
+            } else if (i < 52) {
+                levelBlocks[i].positionX = screenWidth/2 + (i - 32) * 12.0 * Math.cos((elapsed * (i - 32)/100.0))
+                levelBlocks[i].positionY = screenHeight/2 + (i - 32) * 12.0 * Math.sin((elapsed * (i - 32)/100.0))
+            }
+        } 
+    }
+    if (isColliding(playerSprite, rightBorder.graphic)) {
+        isOnGround = false;
+    }
+    leftBorder.graphic.interactive = false;
+    bottomBorder.graphic.interactive = false;
+}
+Level5(delta){
+
+}
+Level6(delta){
     for (let i = 0; i < 7; i++) {
         if (levelBlocks[i]) {
             if (i == 0) {
@@ -92,11 +128,8 @@ class Level{
             }
         }
     }
-  }
-  Level4(delta){
-
-  }
-  Level5(delta){
+}
+Level7(delta){
     if (levelBlocks[4]) {
         levelBlocks[4].positionX = Math.max(playerSprite.x - 100, 400)
     }
@@ -106,9 +139,9 @@ class Level{
         else
             levelBlocks[10].positionX += 3.5 * delta;
     }
-  }
+}
 
-  Level6(delta){
+Level8(delta){
     if (levelBlocks[0]) {
         levelBlocks[0].positionY = Math.max(playerSprite.y + 10, 0)
         levelBlocks[0].positionX = Math.max(playerSprite.x - 100, 400)
@@ -129,7 +162,7 @@ class Level{
     if (levelBlocks[4]) {
         levelBlocks[4].positionY -= 0.7 * delta;
     }
-  }
+}
 
       /*"6": [
         {
@@ -383,42 +416,6 @@ class Level{
         }  
     }
   }*/
-
-    Level7(delta){
-        for (let i = 0; i < 6; i++) {
-            if (levelBlocks[i]) {
-                levelBlocks[i].positionX += 5 * delta;
-            }
-        }
-        for (let i = 6; i < 12; i++) {
-            if (levelBlocks[i]) {
-                levelBlocks[i].positionX -= 5 * delta;
-            }
-        }
-    }
-
-    Level8(delta){
-        elapsed += delta;
-        for (let i = 0; i < 52; i++) {
-            if (levelBlocks[i]) {
-                if (i < 16) {
-                    levelBlocks[i].positionX = i * 20.0 * Math.cos((elapsed * i/100.0))
-                    levelBlocks[i].positionY = i * 20.0 * Math.sin((elapsed * i/100.0))
-                } else if (i < 32) {
-                    levelBlocks[i].positionX = screenWidth + (i - 16) * 20.0 * Math.cos((elapsed * (i - 16)/100.0))
-                    levelBlocks[i].positionY = screenHeight + (i - 16) * 20.0 * Math.sin((elapsed * (i - 16)/100.0))
-                } else if (i < 52) {
-                    levelBlocks[i].positionX = screenWidth/2 + (i - 32) * 12.0 * Math.cos((elapsed * (i - 32)/100.0))
-                    levelBlocks[i].positionY = screenHeight/2 + (i - 32) * 12.0 * Math.sin((elapsed * (i - 32)/100.0))
-                }
-            } 
-        }
-        if (isColliding(playerSprite, rightBorder.graphic)) {
-            isOnGround = false;
-        }
-        leftBorder.graphic.interactive = false;
-        bottomBorder.graphic.interactive = false;
-    }
     InitiateLevel9(){
         cannon = new Cannon(630, 350, 20, 20, 2, "#ff0081", "v", "#ff0081", 10, 20, -90, 90);
         cannonInterval = 0;
